c9d602a0b7d0b669d6316444f12dc8ad119752ec
Mercury Rory <Mercury Rory>
2017-02-12 15:31:15 +0300

    Customed scrollbar, fix a refer in the social section.

--- src/blocks/header/navigation/navigation.js
+++ src/blocks/header/navigation/navigation.js
@@ -5,10 +5,24 @@ $('.anchor').on('click', function(evt) {
   evt.preventDefault();
 });// end click
 // Transition to a section.
+
 $('body').on('click', 'a.item__text, #btnUp', function(event) {
   var $this = $(this);
   $('html, body').stop().animate({scrollTop: $($this.attr('href')).offset().top},1500,'easeInOutExpo');
   event.preventDefault();
 });// end click
+
+var $btnUp = $('#btnUp');
+
+$btnUp.hide();
+
+$(window).scroll(function() {
+  
+  if ($(this).scrollTop() > 150) {
+    $btnUp.fadeIn(700);
+  } else {
+    $btnUp.fadeOut(1000);
+  }
+});// end scroll
 
 $('body').scrollspy({target: ".navbar-fixed-top", offset: 50}); // end scrollspy
