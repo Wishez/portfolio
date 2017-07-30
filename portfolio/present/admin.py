5889dda43fece5908339b485f59113e3b2f69077
Wishez <shiningfinger@list.ru>
2017-07-30 20:25:01 +0300

    New flexible version of my portfolio.

--- portfolio/present/admin.py
+++ portfolio/present/admin.py
@@ -1,8 +1,12 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
-from .models import Order, Work
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Order)
 admin.site.register(Work)
+admin.site.register(Technology)
+admin.site.register(Concept)
+admin.site.register(Tool)
+admin.site.register(MoreTool)
--- portfolio/present/views.py
+++ portfolio/present/views.py
@@ -1,21 +1,30 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render, redirect
 from .forms import ConnectForm
-from .models import Work
+from .models import *
 from django.utils import timezone
-from django.core.mail import send_mail
 from django.http import HttpResponse
+import threading
 # Create your models here.
 
 def index(request):
     form = ConnectForm()
     works = Work.objects.filter(added_date__lte=timezone.now()).order_by('added_date').reverse()
+    technologies = Technology.objects.all()
+    tools = Tool.objects.all()
+    additionaltools = MoreTool.objects.all()
+    concepts = Concept.objects.all()
+
     return render(
         request,
         'index.html',
         {
             'form': form,
-            'works': works
+            'works': works,
+            'technologies': technologies,
+            'tools': tools,
+            'additionaltools': additionaltools,
+            'concepts': concepts
         })
 
 def success(request):
@@ -26,7 +35,8 @@ def success(request):
             order.save()
             orderMsg = request.POST['name'] + '\n' + request.POST['email'] + '\n' + request.POST['phone'] + '\n' +  request.POST['message']
 
-            send_mail('Хочешь поработать?.', orderMsg, 'comandos.testing@list.ru', ['shiningfinger@list.ru'])
+            threading.Thread(target=order.sendMail, args=(orderMsg,)).start()
+
             html = '<h3 class="text-center success__title"> Оставайтесь на связи!:)</h3>' + \
                    "<p class='success__text'>Скоро я зайду в свой почтовый ящик и увижу ваше сообщение, после чего буду рад обсудить с вами то, что вас интересует.</p>"
 
