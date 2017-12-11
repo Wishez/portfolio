c597b3a95e54f5b1192566b661ada3a46956024f
Filipp Zhuravlev <shiningfinger@list.ru>
2017-12-11 17:26:25 +0300

    Add serializers, views, and urls for work model.

--- shining_present/app/urls.py
+++ shining_present/app/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls import url
-
+from .api.views import *
 urlpatterns = [
-
+    url(
+        r'^api/v1/works_list/$',
+        WorkPreviewView.as_view(),
+        name='plugin_user'
+    ),
+    url(
+        r'^api/v1/works/(?P<uuid>[-\w]+)/$',
+        WorkView.as_view(),
+        name='document_data_user'
+    ),
 ]
