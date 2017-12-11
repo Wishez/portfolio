360fa742bd45225b9b46a3e72f7d43d7093bee9f
Wishez <shiningfinger@list.ru>
2017-12-11 17:23:49 +0300

    Change models for my needs.

--- shining_present/album/admin.py
+++ shining_present/album/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import uuid
 import zipfile
-import sauna.settings
+import shining_present.settings
 from datetime import datetime
 from zipfile import ZipFile
 
@@ -33,23 +33,24 @@ class AlbumModelAdmin(admin.ModelAdmin):
                     contentfile = ContentFile(data)
 
                     img = AlbumImage()
-                    img.album = album
+                    # img.album = album
                     img.alt = filename
                     filename = '{0}{1}.jpg'.format(album.slug, str(uuid.uuid4())[-13:])
                     img.image.save(filename, contentfile)
 
-                    filepath = '{0}/albums/{1}'.format(sauna.settings.MEDIA_ROOT, filename)
+                    filepath = '{0}/albums/{1}'.format(shining_present.settings.MEDIA_ROOT, filename)
                     with Image.open(filepath) as i:
                         img.width, img.height = i.size
                     img.save()
 
+                    album.album_images.add(img)
                     #img.thumb.save('thumb-{0}'.format(filename), contentfile)
-
+                album.save()
                 zip.close()
             super(AlbumModelAdmin, self).save_model(request, obj, form, change)
 
 # In case image should be removed from album.
 @admin.register(AlbumImage)
 class AlbumImageModelAdmin(admin.ModelAdmin):
-    list_display = ('alt', 'album')
-    list_filter = ('album', 'created')
+    list_display = ('alt',)
+    list_filter = ('created',)
