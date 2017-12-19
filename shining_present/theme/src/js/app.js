cbe6a703449a18d39bc5a6c3c0f22691696f835a
Wishez <shiningfinger@list.ru>
2017-12-19 23:02:42 +0300

    Add lazy loading for images at the work page.

--- shining_present/theme/src/js/app.js
+++ shining_present/theme/src/js/app.js
@@ -1,10 +1,20 @@
 // import {TimelieMax} from 'gsap';
 import $ from 'jquery';
+import lozad from 'lozad';
 
 (function() {
   const _screwed = (selector, callback, event='click') => {
     $(document).on(event, selector, callback);
   };
+  lozad('.lozad', {
+    load: function(el) {
+      el.src = el.dataset.src;
+      el.onload = function() {
+        el.classList.add('in');
+      };
+    }
+  }).observe();
+  // lozad('' ,function() {});
 
   $(function() {
     // let tl = new TimelieMax();
