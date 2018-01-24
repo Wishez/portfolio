261d443799c00632a7f1587e514e22485faf0f16
Wishez <shiningfinger@list.ru>
2018-01-24 21:45:36 +0300

    Add created pages' views and registered urls for these views.

--- shining_present/pages/urls.py
+++ shining_present/pages/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from .views import *
 urlpatterns = [
     url('^$', PortfolioView.as_view(), name='portfolio'),
-    # url('^about/$', AboutView.as_view(), name='about'),
-    # url('^connect/$', ConnectView.as_view(), name='connect'),
-    # url('^articles/$', ArticleView.as_view(), name='contacts'),
-    # url('^work/(?P<slug>[a-z_\-0-9]+)/$', WorkView.as_view(), name='work'),
-    # url('^article/(?P<slug>[a-z_\-0-9]+)/$', ArticleView.as_view(), name='article'),
+    url('^about/$', AboutPageView.as_view(), name='about'),
+    url('^connect/$', TechnologiesView.as_view(), name='connect'),
+    url('^articles/$', ArticlesPageView.as_view(), name='articles'),
+    url('^work/(?P<slug>[a-z_\-0-9]+)/$', WorkView.as_view(), name='work'),
+    url('^article/(?P<slug>[a-z_\-0-9]+)/$', ArticlePageView.as_view(), name='article'),
 ]
