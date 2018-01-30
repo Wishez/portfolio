22b9e527d66b9cee042e039bbc81b34d7fd4ed1d
Wishez <shiningfinger@list.ru>
2018-01-30 22:29:21 +0300

    Add pages' views with their variables/

--- shining_present/pages/views.py
+++ shining_present/pages/views.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-
+from app.forms import ConnectForm
 from app.models import Settings, Work
 from .models import *
 # Create your views here.
@@ -20,7 +20,8 @@ class BaseView(TemplateView):
 
         return context
     def get_page(self):
-        self.page = self.page_model.objects.get()
+        if self.page_model is not None:
+            self.page = self.page_model.objects.get()
 
     def get_context_data(self, **kwargs):
         context = super(BaseView, self).get_context_data(**kwargs)
@@ -46,6 +47,10 @@ class PortfolioView(BaseView):
         super(PortfolioView, self).__init__()
         self.page_model = PortfolioPage
 
+    def set_additional_context(self, context):
+        works = self.page.works.all()
+        context['works'] = [work for work in works if work in works]
+        return context
 
 class TechnologiesView(BaseView):
     template_name = 'technologies.html'
@@ -53,9 +58,12 @@ class TechnologiesView(BaseView):
     def __init__(self):
         super(TechnologiesView, self).__init__()
         self.page_model = TechnologiesPage
+    def set_additional_context(self, context):
+        context['form'] = ConnectForm()
+
+        return context
 class AboutPageView(BaseView):
     template_name = 'about.html'
-
     def __init__(self):
         super(AboutPageView, self).__init__()
         self.page_model = AboutPage
@@ -69,15 +77,47 @@ class ArticlesPageView(BaseView):
 
 class WorkView(BaseView):
     template_name = 'work.html'
+    def __init__(self):
+        super(WorkView, self).__init__()
+    def set_additional_context(self, context):
+        works = Work.objects.all()
+        currentIndex = None
+        next = None
+        last = None
+        quantityWorks = works.count()
+
+        for i in range(len(works)):
+            if works[i].id == self.page.id:
+                currentIndex = i
+
+        if quantityWorks == 1:
+            pass
+        elif quantityWorks == 2:
+            lastIndex = 0
+            if currentIndex == 0:
+                lastIndex += 1
+
+            last = works[lastIndex]
+        elif currentIndex == 0 and quantityWorks >= 3:
+            last = works[quantityWorks - 1]
+            next = works[currentIndex + 1]
+        else:
+            last = works[currentIndex - 1]
+            next = works[currentIndex + 1]
+
+        context['last'] = last
+        context['next'] = next
+
+        return context
 
     def get(self, request, slug):
         self.page = get_object_or_404(Work, slug=slug)
 
-        return super(Work, self).get(request)
+        return super(WorkView, self).get(request)
 
 class ArticlePageView(BaseView):
     template_name = 'article.html'
-    page_model = ArticlesPage
+
     def __init__(self):
         super(ArticlePageView, self).__init__()
         self.page_model = ArticlePage
