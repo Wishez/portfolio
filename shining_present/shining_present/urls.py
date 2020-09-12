fae180fbbe6228948936efac749d6edbf839d08c
Filipp Zhuravlev <shiningfinger@list.ru>
2020-09-12 22:04:02 +0300

    Пофиксил загрузку статических файлов

--- shining_present/shining_present/urls.py
+++ shining_present/shining_present/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles import views
+
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),
