3566c9d8f23052167a49b46447fe95a38666269c
Wishez <shiningfinger@list.ru>
2020-09-19 23:47:50 +0300

    Добавил минификацию для хтмл файлов

--- shining_present/theme/gulp/tasks/jade.js
+++ shining_present/theme/gulp/tasks/jade.js
@@ -5,6 +5,7 @@ var changed     = require('gulp-changed');
 var gulpif      = require('gulp-if');
 var frontMatter = require('gulp-front-matter');
 var prettify    = require('gulp-prettify');
+var htmlmin    = require('gulp-htmlmin');
 var config      = require('../config');
 
 function renderHtml(onlyChanged) {
@@ -14,14 +15,18 @@ function renderHtml(onlyChanged) {
         .pipe(gulpif(onlyChanged, changed(config.dest.html, { extension: '.html' })))
         .pipe(frontMatter({ property: 'data' }))
         .pipe(jade())
-        .pipe(prettify({
-            indent_size: 2,
-            wrap_attributes: 'auto', // 'force'
-            preserve_newlines: true,
-            pretty: true,
-            // unformatted: [],
-            end_with_newline: true
-        }))
+        .pipe(
+            config.production
+                ? htmlmin({ collapseWhitespace: true })
+                : prettify({
+                    indent_size: 2,
+                    wrap_attributes: 'auto', // 'force'
+                    preserve_newlines: true,
+                    pretty: true,
+                    // unformatted: [],
+                    end_with_newline: true
+                })
+        )
         .pipe(gulp.dest(config.dest.html));
 }
 
