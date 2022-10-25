73bf0cdea3f9509ed4edcb0a624f699b413a7336
Phil Zhuravlev <shiningfinger@list.ru>
2022-10-25 18:53:43 +0300

    Поправил отображение работ на главной

--- shining_present/pages/urls.py
+++ shining_present/pages/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from .views import *
+
 urlpatterns = [
     url('^$', PortfolioView.as_view(), name='portfolio'),
     url('^about/$', AboutPageView.as_view(), name='about'),
-    url('^connect/$', TechnologiesView.as_view(), name='connect'),
+    url('^connect/$', ConnectMeView.as_view(), name='connect'),
+    url('^tools/$', TechnologiesView.as_view(), name='tools'),
     url('^articles/$', ArticlesPageView.as_view(), name='articles'),
     url('^work/(?P<slug>[a-z_\-0-9]+)/$', WorkView.as_view(), name='work'),
     url('^article/(?P<slug>[\w\-]+)/$', ArticlePageView.as_view(), name='article'),
